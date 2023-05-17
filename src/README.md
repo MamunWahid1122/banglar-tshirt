ba732bf6beab5ed842f13630f88679714b604097
MamunWahid1122 <mdmamunkhanball75@gmail.com>
2023-05-17 22:27:27 +0600

    intial data load

---
+++ src/README.md
@@ -0,0 +1,1 @@
+### Bangalr T-Shirt
---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <he>Order Review</he>
+        </div>
+    );
+};
+
+export default OrderReview;
